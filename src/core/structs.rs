78e2876ddb4d74d90723be16854f154ad31bd428
Andrei <drop.all.requests@gmail.com>
2024-12-31 06:26:53 +0300

    Update structs.rs

--- src/core/structs.rs
+++ src/core/structs.rs
@@ -1,26 +1,34 @@
 use std::collections::HashMap;
 
+use serde::{Deserialize, Serialize};
+use serde_json;
+
 use crate::core::constants::SOFT_ID;
 use crate::core::enums::TaskType;
 
+#[derive(Serialize, Deserialize)]
 pub struct CreateTaskRequest {
     clientKey: String,
     task: HashMap<String, String>,
     softId: String,
-    callbackUrl: Option<String>,
+    callbackUrl: String,
 }
 impl CreateTaskRequest {
-    pub fn new(
-        clientKey: String,
-        task: HashMap<String, String>,
-        callbackUrl: Option<String>,
-    ) -> Self {
+    pub fn new(clientKey: String, task: HashMap<String, String>, callbackUrl: String) -> Self {
         CreateTaskRequest {
             clientKey,
             task,
             softId: SOFT_ID.to_string(),
             callbackUrl,
         }
+    }
+    pub fn into_json(self) -> String {
+        // method transform create task structure fields into json
+        let json_string = match serde_json::to_string(&self) {
+            Ok(json_string) => json_string,
+            Err(error) => panic!("Error serializing CreateTaskRequest to JSON: {}", error),
+        };
+        json_string
     }
 }
 
