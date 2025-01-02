84e63106ffeff96efe076521a1b4a34043f08f27
Andrei <drop.all.requests@gmail.com>
2025-01-02 03:46:35 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,28 @@ use std::collections::HashMap;
 mod control;
 mod core;
 
-use crate::core::enums::{ControlEnpPostfix, TaskType};
-use core::client::Client;
+use crate::control::Control;
 
 const API_KEY: &str = "999999999999999999999999999";
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    println!("Hello, world!");
     let client = reqwest::Client::new();
     let response = client.get("https://www.rust-lang.org").send().await?;
     println!("status code is = {}", response.status());
 
-    let mut captcha_client = Client::new(API_KEY.to_string());
-
-    let mut captcha_params: HashMap<String, String> = HashMap::new();
-    captcha_params.insert(
-        "websiteURL".to_string(),
-        "https://docs.rs/reqwest/0.12.11/reqwest/".to_string(),
-    );
-    captcha_params.insert("websiteKey".to_string(), "3htd36tpbin543".to_string());
-    captcha_params.insert("recaptchaDataSValue".to_string(), "".to_string());
-    captcha_params.insert("isInvisible".to_string(), false.to_string());
-    captcha_client.solve_captcha(TaskType::RecaptchaV2TaskProxyless, captcha_params);
-
     let mut map: HashMap<String, String> = HashMap::new();
     map.insert("clientKey".to_string(), API_KEY.to_string());
 
-    let post_result = captcha_client
-        .send_post_request(&map, &ControlEnpPostfix::getBalance)
-        .await;
+    let control_client = Control::new(API_KEY.to_string());
 
-    println!("{}", post_result);
+    let control_result: serde_json::Value = control_client.get_balance().await;
+    println!("Your balance - {:?}", control_result);
+
+    let mut map: HashMap<String, String> = HashMap::new();
+    map.insert("queueId".to_string(), 6.to_string());
+    let control_result: serde_json::Value = control_client.get_queue_stats(&map).await;
+    println!("Queue stats - {:?}", control_result);
 
     Ok(())
 }
