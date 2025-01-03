b2a05bfbe39d6b00b54d69e1611531b5382bded8
Andrei <drop.all.requests@gmail.com>
2025-01-04 02:03:24 +0300

    Update enums.rs

--- src/core/enums.rs
+++ src/core/enums.rs
@@ -1,20 +1,56 @@
 pub enum TaskType {
     ImageToTextTask,
-    RecaptchaV2TaskProxyless,
+    ImageToCoordinatesTask,
+
     RecaptchaV2Task,
+    RecaptchaV2TaskProxyless,
+
+    RecaptchaV3Enterprise,
+    RecaptchaV3TaskProxyless,
+
+    RecaptchaV2EnterpriseTask,
+    RecaptchaV2EnterpriseTaskProxyless,
+
+    FunCaptchaTask,
+    FunCaptchaTaskProxyless,
+
+    GeeTestTask,
+    GeeTestTaskProxyless,
+
+    TurnstileTask,
+    TurnstileTaskProxyless,
+
+    AntiGateTask,
 }
 
 impl TaskType {
     pub fn value_as_string(&self) -> String {
         match &self {
             TaskType::ImageToTextTask => "ImageToTextTask".to_string(),
-            TaskType::RecaptchaV2TaskProxyless => "RecaptchaV2TaskProxyless".to_string(),
+            TaskType::ImageToCoordinatesTask => "ImageToCoordinatesTask".to_string(),
             TaskType::RecaptchaV2Task => "RecaptchaV2Task".to_string(),
+            TaskType::RecaptchaV2TaskProxyless => "RecaptchaV2TaskProxyless".to_string(),
+            TaskType::RecaptchaV3Enterprise => "RecaptchaV3Enterprise".to_string(),
+            TaskType::RecaptchaV3TaskProxyless => "RecaptchaV3TaskProxyless".to_string(),
+            TaskType::RecaptchaV2EnterpriseTask => "RecaptchaV2EnterpriseTask".to_string(),
+            TaskType::RecaptchaV2EnterpriseTaskProxyless => {
+                "RecaptchaV2EnterpriseTaskProxyless".to_string()
+            }
+            TaskType::FunCaptchaTask => "FunCaptchaTask".to_string(),
+            TaskType::FunCaptchaTaskProxyless => "FunCaptchaTaskProxyless".to_string(),
+            TaskType::GeeTestTask => "GeeTestTask".to_string(),
+            TaskType::GeeTestTaskProxyless => "GeeTestTaskProxyless".to_string(),
+            TaskType::TurnstileTask => "TurnstileTask".to_string(),
+            TaskType::TurnstileTaskProxyless => "TurnstileTaskProxyless".to_string(),
+            TaskType::AntiGateTask => "AntiGateTask".to_string(),
         }
     }
 }
 
-pub enum ControlEnpPostfix {
+pub enum EnpPostfix {
+    // tasks processing
+    createTask,
+    getTaskResult,
     // get account info
     getBalance,
     getQueueStats,
@@ -28,20 +64,20 @@ pub enum ControlEnpPostfix {
     pushAntiGateVariable,
 }
 
-impl ControlEnpPostfix {
+impl EnpPostfix {
     pub fn value_as_string(&self) -> String {
         match &self {
-            ControlEnpPostfix::getBalance => "getBalance".to_string(),
-            ControlEnpPostfix::getQueueStats => "getQueueStats".to_string(),
-            ControlEnpPostfix::getAppStats => "getAppStats".to_string(),
-            ControlEnpPostfix::getSpendingStats => "getSpendingStats".to_string(),
-            ControlEnpPostfix::reportIncorrectImageCaptcha => {
-                "reportIncorrectImageCaptcha".to_string()
-            }
-            ControlEnpPostfix::reportIncorrectRecaptcha => "reportIncorrectRecaptcha".to_string(),
-            ControlEnpPostfix::reportCorrectRecaptcha => "reportCorrectRecaptcha".to_string(),
-            ControlEnpPostfix::reportIncorrectHcaptcha => "reportIncorrectHcaptcha".to_string(),
-            ControlEnpPostfix::pushAntiGateVariable => "pushAntiGateVariable".to_string(),
+            EnpPostfix::createTask => "createTask".to_string(),
+            EnpPostfix::getTaskResult => "getTaskResult".to_string(),
+            EnpPostfix::getBalance => "getBalance".to_string(),
+            EnpPostfix::getQueueStats => "getQueueStats".to_string(),
+            EnpPostfix::getAppStats => "getAppStats".to_string(),
+            EnpPostfix::getSpendingStats => "getSpendingStats".to_string(),
+            EnpPostfix::reportIncorrectImageCaptcha => "reportIncorrectImageCaptcha".to_string(),
+            EnpPostfix::reportIncorrectRecaptcha => "reportIncorrectRecaptcha".to_string(),
+            EnpPostfix::reportCorrectRecaptcha => "reportCorrectRecaptcha".to_string(),
+            EnpPostfix::reportIncorrectHcaptcha => "reportIncorrectHcaptcha".to_string(),
+            EnpPostfix::pushAntiGateVariable => "pushAntiGateVariable".to_string(),
         }
     }
 }
