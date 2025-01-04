86c1fc82340061432e178a9b7a6af9a8912a1e22
Andrei <drop.all.requests@gmail.com>
2025-01-04 23:03:57 +0300

    Update constants.rs

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -1,4 +1,5 @@
-const SOFT_ID: &str = "867";
 const CREATE_TASK_POSTFIX: &str = "/createTask";
 const GET_RESULT_POSTFIX: &str = "/getTaskResult";
+
+pub const SOFT_ID: &str = "867";
 pub const BASE_REQUEST_URL: &str = "https://api.anti-captcha.com/";
