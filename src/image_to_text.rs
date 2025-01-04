320853cce35370ab8d8d1b358c020bd308653f8b
Andrei <drop.all.requests@gmail.com>
2025-01-04 04:42:51 +0300

    Update image_to_text.rs

--- src/image_to_text.rs
+++ src/image_to_text.rs
@@ -3,52 +3,52 @@ use std::collections::HashMap;
 use super::core::captcha_interface::CaptchaInterface;
 use super::core::enums::TaskType;
 
+/// Captcha solving method - `ImageToTextTask `
+///
+/// # Examples
+///
+/// ```
+/// let mut map: HashMap<String, String> = HashMap::new();
+/// map.insert("body".to_string(), image_file_base64);
+/// let image_to_text_client = ImageToText::new(API_KEY);
+/// let result = image_to_text_client.captcha_handler(&map);
+/// ```
+///
+/// # Notes
+/// Read more here:
+///
+/// <https://anti-captcha.com/apidoc/task-types/ImageToTextTask>
 pub struct ImageToText {
-    /// Captcha solving method - `ImageToTextTask `
+    pub captcha_interface: CaptchaInterface,
+}
+impl ImageToText {
+    /// Method init new ImageToText struct with Captcha Interface
     ///
     /// # Examples
     ///
     /// ```
     /// let image_to_text_client = ImageToText::new(API_KEY);
-    /// let mut map: HashMap<String, String> = HashMap::new();
-    /// map.insert("queueId".to_string(), 6.to_string());
-    /// let result: serde_json::Value = control_client.get_queue_stats(&map).await;
     /// ```
-    ///
-    /// # Notes
-    /// Read more here:
-    ///
-    /// https://anti-captcha.com/apidoc/task-types/ImageToTextTask
-    pub captcha_interface: CaptchaInterface,
-}
-impl ImageToText {
     pub fn new(api_key: String) -> Self {
-        /// Method init new ImageToText struct with Captcha Interface
-        ///
-        /// # Examples
-        ///
-        /// ```
-        /// let image_to_text_client = ImageToText::new(API_KEY);
-        /// ```
         ImageToText {
             captcha_interface: CaptchaInterface::new(api_key),
         }
     }
 
+    /// Method run captcha solving logic
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut map: HashMap<String, String> = HashMap::new();
+    /// map.insert("body".to_string(), image_file_base64);
+    /// let image_to_text_client = ImageToText::new(API_KEY);
+    /// let result = image_to_text_client.captcha_handler(&map);
+    /// ```
     pub async fn captcha_handler(
         &mut self,
         task_payload: &HashMap<String, String>,
     ) -> serde_json::Value {
-        /// Method run captcha solving logic
-        ///
-        /// # Examples
-        ///
-        /// ```
-        /// let mut map: HashMap<String, String> = HashMap::new();
-        /// map.insert("body".to_string(), image_file_base64);
-        /// let image_to_text_client = ImageToText::new(API_KEY);
-        /// let result = image_to_text_client.captcha_handler();
-        /// ```
         self.captcha_interface
             .solve_captcha(TaskType::ImageToTextTask, task_payload.clone())
             .await
