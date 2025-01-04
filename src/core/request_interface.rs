2455b1b179091040687e16931fe39e3090a967fa
Andrei <drop.all.requests@gmail.com>
2025-01-04 03:03:07 +0300

    Update request_interface.rs

--- src/core/request_interface.rs
+++ src/core/request_interface.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
+
 use super::constants::BASE_REQUEST_URL;
 use super::enums::EnpPostfix;
-use crate::core::structs::CreateTaskRequest;
-use std::collections::HashMap;
+use super::structs::{CreateTaskRequest, ResultTaskRequest};
 
 pub struct RequestInterface {
     task_payload: HashMap<String, String>,
@@ -41,6 +42,30 @@ impl RequestInterface {
     pub async fn send_create_task_request(
         &self,
         payload: &CreateTaskRequest,
+        enp_postfix: &EnpPostfix,
+    ) -> serde_json::Value {
+        let req_url = BASE_REQUEST_URL.to_string() + &enp_postfix.value_as_string();
+
+        let response = self
+            .request_client
+            .post(req_url)
+            .json(payload)
+            .send()
+            .await
+            .unwrap();
+
+        if response.status() != 200 {
+            panic!(
+                "Invalid request to API, response - {}",
+                response.text().await.unwrap()
+            )
+        } else {
+            response.json().await.unwrap()
+        }
+    }
+    pub async fn send_get_result_request(
+        &self,
+        payload: &ResultTaskRequest,
         enp_postfix: &EnpPostfix,
     ) -> serde_json::Value {
         let req_url = BASE_REQUEST_URL.to_string() + &enp_postfix.value_as_string();
