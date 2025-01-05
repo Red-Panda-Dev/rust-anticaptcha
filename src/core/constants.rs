282483143585f63f2966b6637885757b10fb8c2a
Andrei <drop.all.requests@gmail.com>
2025-01-06 02:55:33 +0300

    Update constants.rs

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -1,2 +1,2 @@
-pub const SOFT_ID: &str = "1220";
-pub const BASE_REQUEST_URL: &str = "https://api.anti-captcha.com/";
+pub const SOFT_ID: &'static str = "1220";
+pub const BASE_REQUEST_URL: &'static str = "https://api.anti-captcha.com/";
