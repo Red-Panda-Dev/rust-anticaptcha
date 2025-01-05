3a70a5b07b2d162af14e20dc12d1711482be2c68
Andrei <drop.all.requests@gmail.com>
2025-01-05 22:09:49 +0300

    Update mod.rs

--- src/instruments/mod.rs
+++ src/instruments/mod.rs
@@ -1,1 +1,8 @@
+//! # Instrument module
+//!
+//! This module contains useful instruments for preparing data for solving captcha.
+//!
+//! - [ImageInstrument][image_instrument::ImageInstrument]
+//!
+
 pub mod image_instrument;
