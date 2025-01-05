9b08fb91308fdaf6388887762376576bb7685d40
Andrei <drop.all.requests@gmail.com>
2025-01-05 18:27:56 +0300

    Update image_captcha_test.rs

--- tests/image_captcha_test.rs
+++ tests/image_captcha_test.rs
@@ -1,3 +1,6 @@
+use serde_json::json;
+
+use rust_anticaptcha::core::enums::{GetResultStatus, ImageTaskType};
 use rust_anticaptcha::image_captcha::ImageCaptcha;
 use rust_anticaptcha::instruments::image_instrument::ImageInstrument;
 
@@ -57,4 +60,37 @@ fn image_instrument_read_coord_image_file() {
     assert!(instance
         .read_image_file(captcha_args.coord_captcha_file)
         .ends_with(&captcha_args.coord_captcha_ends_with));
+}
+
+#[tokio::test]
+async fn success_image_file() {
+    let captcha_args = ImageCaptchaArgs::new();
+
+    let instance = ImageInstrument::new();
+    let base64_str = instance.read_image_file(captcha_args.captcha_file);
+
+    let mut solver_instance = ImageCaptcha::new(captcha_args.API_KEY);
+
+    let result = solver_instance
+        .captcha_handler(ImageTaskType::ImageToTextTask, json!({"body":base64_str}))
+        .await;
+    assert_eq!(result["errorId"], 0);
+    assert_eq!(result["status"], GetResultStatus::ready.value_as_string());
+}
+
+#[tokio::test]
+async fn fail_image_file() {
+    let captcha_args = ImageCaptchaArgs::new();
+
+    let instance = ImageInstrument::new();
+    let base64_str = instance.read_image_file(captcha_args.captcha_file);
+
+    let mut solver_instance = ImageCaptcha::new("API_KEY".to_string());
+
+    let result = solver_instance
+        .captcha_handler(ImageTaskType::ImageToTextTask, json!({"body":base64_str}))
+        .await;
+
+    assert_eq!(result["errorId"], 1);
+    assert_eq!(result["errorCode"], "ERROR_KEY_DOES_NOT_EXIST");
 }
