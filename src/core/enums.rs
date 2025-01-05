500455e56b18ed4a2146252a238f5972b76c1507
Andrei <drop.all.requests@gmail.com>
2025-01-05 20:01:09 +0300

    Update enums.rs

--- src/core/enums.rs
+++ src/core/enums.rs
@@ -1,19 +1,26 @@
+use std::fmt;
+
 pub trait TaskTypeTrait {
-    fn value_as_string(&self) -> String;
+
+    fn as_string(&self) -> String;
 }
 
+#[derive(Debug)]
 pub enum ImageTaskType {
     ImageToTextTask,
     ImageToCoordinatesTask,
 }
 impl TaskTypeTrait for ImageTaskType {
-    fn value_as_string(&self) -> String {
-        match &self {
-            ImageTaskType::ImageToTextTask => "ImageToTextTask".to_string(),
-            ImageTaskType::ImageToCoordinatesTask => "ImageToCoordinatesTask".to_string(),
-        }
+    fn as_string(&self) -> String {
+        format!("{}", &self)
+    }}
+impl fmt::Display for ImageTaskType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
+
+#[derive(Debug)]
 pub enum TokenTaskType {
     RecaptchaV2Task,
     RecaptchaV2TaskProxyless,
@@ -35,29 +42,17 @@ pub enum TokenTaskType {
 
     AntiGateTask,
 }
-
 impl TaskTypeTrait for TokenTaskType {
-    fn value_as_string(&self) -> String {
-        match &self {
-            TokenTaskType::RecaptchaV2Task => "RecaptchaV2Task".to_string(),
-            TokenTaskType::RecaptchaV2TaskProxyless => "RecaptchaV2TaskProxyless".to_string(),
-            TokenTaskType::RecaptchaV3Enterprise => "RecaptchaV3Enterprise".to_string(),
-            TokenTaskType::RecaptchaV3TaskProxyless => "RecaptchaV3TaskProxyless".to_string(),
-            TokenTaskType::RecaptchaV2EnterpriseTask => "RecaptchaV2EnterpriseTask".to_string(),
-            TokenTaskType::RecaptchaV2EnterpriseTaskProxyless => {
-                "RecaptchaV2EnterpriseTaskProxyless".to_string()
-            }
-            TokenTaskType::FunCaptchaTask => "FunCaptchaTask".to_string(),
-            TokenTaskType::FunCaptchaTaskProxyless => "FunCaptchaTaskProxyless".to_string(),
-            TokenTaskType::GeeTestTask => "GeeTestTask".to_string(),
-            TokenTaskType::GeeTestTaskProxyless => "GeeTestTaskProxyless".to_string(),
-            TokenTaskType::TurnstileTask => "TurnstileTask".to_string(),
-            TokenTaskType::TurnstileTaskProxyless => "TurnstileTaskProxyless".to_string(),
-            TokenTaskType::AntiGateTask => "AntiGateTask".to_string(),
-        }
+    fn as_string(&self) -> String {
+        format!("{}", &self)
+    }}
+impl fmt::Display for TokenTaskType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 
+#[derive(Debug)]
 pub enum EnpPostfix {
     // tasks processing
     createTask,
@@ -74,37 +69,20 @@ pub enum EnpPostfix {
     reportIncorrectHcaptcha,
     pushAntiGateVariable,
 }
-
-impl EnpPostfix {
-    pub fn value_as_string(&self) -> String {
-        match &self {
-            EnpPostfix::createTask => "createTask".to_string(),
-            EnpPostfix::getTaskResult => "getTaskResult".to_string(),
-            EnpPostfix::getBalance => "getBalance".to_string(),
-            EnpPostfix::getQueueStats => "getQueueStats".to_string(),
-            EnpPostfix::getAppStats => "getAppStats".to_string(),
-            EnpPostfix::getSpendingStats => "getSpendingStats".to_string(),
-            EnpPostfix::reportIncorrectImageCaptcha => "reportIncorrectImageCaptcha".to_string(),
-            EnpPostfix::reportIncorrectRecaptcha => "reportIncorrectRecaptcha".to_string(),
-            EnpPostfix::reportCorrectRecaptcha => "reportCorrectRecaptcha".to_string(),
-            EnpPostfix::reportIncorrectHcaptcha => "reportIncorrectHcaptcha".to_string(),
-            EnpPostfix::pushAntiGateVariable => "pushAntiGateVariable".to_string(),
-        }
+impl fmt::Display for EnpPostfix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 
+#[derive(Debug)]
 pub enum GetResultStatus {
     processing,
     ready,
     error,
 }
-
-impl GetResultStatus {
-    pub fn value_as_string(&self) -> String {
-        match &self {
-            GetResultStatus::processing => "processing".to_string(),
-            GetResultStatus::ready => "ready".to_string(),
-            GetResultStatus::error => "error".to_string(),
-        }
+impl fmt::Display for GetResultStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
